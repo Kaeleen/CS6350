5e7be0408731d8e7faf66c36cb00a33989ee83ea
Kaeleen <kaeleen1211@gmail.com>
2021-09-21 18:01:38 -0600

    re-organized code

--- hw2/ID3.py
+++ hw2/ID3.py
@@ -56,6 +56,9 @@ class ID3:
 		self.option = option
 
 	def calc_entropy(self, data, label_dict):
+		"""
+		This function returns entropy for a specific data subsets and a set of labels
+		"""
 		label_key = list(label_dict.keys())[0]
 		label_values = label_dict[label_key]
 		if len(data) == 0:
@@ -68,6 +71,9 @@ class ID3:
 		return entropy
 	
 	def calc_ME(self, data, label_dict):
+		"""
+		This function returns ME for a specific data subsets and a set of labels
+		"""
 		label_key = list(label_dict.keys())[0]
 		label_values = label_dict[label_key]
 		if len(data) == 0:
@@ -80,6 +86,9 @@ class ID3:
 		
 	
 	def calc_GI(self, data, label_dict):
+		"""
+		This function returns GI for a specific data subsets and a set of labels
+		"""
 		label_key = list(label_dict.keys())[0]
 		label_values = label_dict[label_key]
 		if len(data) == 0:
@@ -91,16 +100,17 @@ class ID3:
 		return 1 - temp
 	
 
-	def main_split(self, cur_node):
-		next_nodes = []
-		features_dict = cur_node['features_dict']
-		label_dict = cur_node['label_dict']
-		dt_node = cur_node['dt_node']
-		data = cur_node['data']
+	def get_majority_label(self,column):
+		"""
+		This function returns the major label
+		"""
 
-		
-		# calcualte heuristics for futher splits
-		
+		majority_label = column.value_counts().idxmax()
+
+		return majority_label
+
+	def get_heuristics(self):
+
 		if self.option == 0:
 			heuristics = self.calc_entropy
 		if self.option == 1:
@@ -108,21 +118,13 @@ class ID3:
 		if self.option == 2:
 			heuristics = self.calc_GI
 
-		
-		label_key = list(label_dict.keys())[0]
-		label_values = label_dict[label_key]
-		
-		if len(data) > 0:
-			majority_label = data[label_key].value_counts().idxmax()
+		return heuristics
 
+
+	def get_feature_with_max_gain(self, data, label_dict, features_dict):
+
+		heuristics = self.get_heuristics()
 		measure = heuristics(data, label_dict)
-
-		# check leaf nodes
-		if measure == 0 or dt_node.get_depth() == self.max_depth or len(features_dict) == 0:
-			dt_node.set_leaf(True)
-			if len(data) > 0:
-				dt_node.set_label(majority_label)
-			return next_nodes
 
 		max_gain = float('-inf')
 		max_f_name = ''
@@ -132,6 +134,7 @@ class ID3:
 			for val in f_values:
 				subset = data[data[f_name] == val]
 				p = len(subset.index) / len(data)
+				
 				gain += p * heuristics(subset, label_dict)
 
 			# get maximum gain and feature name	
@@ -139,8 +142,37 @@ class ID3:
 			if gain > max_gain:
 				max_gain = gain
 				max_f_name = f_name
+
+		return max_f_name
+		
+
+	def best_feature_split(self, cur_node):
+		next_nodes = []
+		features_dict = cur_node['features_dict']
+		label_dict = cur_node['label_dict']
+		dt_node = cur_node['dt_node']
+		data = cur_node['data']
+
+		
+		label_key = list(label_dict.keys())[0]
+		label_values = label_dict[label_key]
+		
+		if len(data) > 0:
+			majority_label = self.get_majority_label(data[label_key])
+			
+		heuristics = self.get_heuristics()
+		measure = heuristics(data, label_dict)
+
+		# check leaf nodes
+		if measure == 0 or dt_node.get_depth() == self.max_depth or len(features_dict) == 0:
+			dt_node.set_leaf(True)
+			if len(data) > 0:
+				dt_node.set_label(majority_label)
+			return next_nodes
+
 		
 		children = {}
+		max_f_name = self.get_feature_with_max_gain(data, label_dict, features_dict)
 		dt_node.set_feature(max_f_name)
 
 		# remove the feature that has been splitted on, get remaining features
@@ -164,7 +196,7 @@ class ID3:
 	# construct the decision tree
 	def construct_dt(self, data, features_dict, label_dict):
 
-		# use queue and bfs
+		# bfs using queue
 		import queue
 		dt_root = TreeNode()
 		dt_root.set_depth(0)
@@ -174,7 +206,7 @@ class ID3:
 		Q.put(root)
 		while not Q.empty():
 			cur_node = Q.get()
-			for node in self.main_split(cur_node):
+			for node in self.best_feature_split(cur_node):
 				Q.put(node)
 		return dt_root
 	
